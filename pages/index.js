a606df4279860c28b3177b546721506cedbe8251
ShaharMS <shaharmsecond@gmail.com>
2022-04-19 23:09:22 +0300

    remove image tag

--- pages/index.js
+++ pages/index.js
@@ -60,7 +60,6 @@ export default function Home() {
         >
           Powered by{' '}
           <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
