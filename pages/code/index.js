a1e074a4fede199403e8359433cfdc7faa69db3a
ShaharMS <shaharmsecond@gmail.com>
2022-04-24 11:43:52 +0300

    fix

--- pages/code/index.js
+++ pages/code/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Script from 'next/script'
 import Link from 'next/link'
 import styles from '../../styles/Home.module.css'
+import space from '../styles/SpaceBG.module.css'
 
 export default function Home() {
   return ( 
