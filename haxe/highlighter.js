2efd23f6a65209cde0f17afb6992017b1cdeb3ef
ShaharMS <shaharmsecond@gmail.com>
2022-05-24 11:34:48 +0300

    highlighting

--- haxe/highlighter.js
+++ haxe/highlighter.js
@@ -26,14 +26,16 @@ Highlighter.hasClass = function(el,className) {
 	return el.className.indexOf(className) != -1;
 };
 Highlighter.syntaxHighlight = function(html) {
-	var kwds = ["abstract","trace","break","case","cast","class","continue","default","do","dynamic","else","enum","extends","extern","for","function","if","implements","import","in","inline","interface","macro","new","override","package","private","public","return","static","switch","throw","try","typedef","untyped","using","var","while"];
-	var kwds1 = new EReg("\\b(" + kwds.join("|") + ")\\b","g");
-	var vals = ["null","true","false","this"];
-	var vals1 = new EReg("\\b(" + vals.join("|") + ")\\b","g");
+	var keywords1 = ["import","break","case","cast","continue","default","else","for","return","switch","throw","try","using","while","as","if"];
+	var keywords1EReg = new EReg("\\b(" + keywords1.join("|") + ")\\b","g");
+	var keywords2 = ["this","abstract","trace","cast","class","dynamic","enum","extends","extern","function","implements","inline","interface","macro","new","override","package","private","public","static","typedef","true","false","null","untyped","var","final","super"];
+	var keywords2EReg = new EReg("\\b(" + keywords2.join("|") + ")\\b","g");
 	var types = new EReg("\\b([A-Z][a-zA-Z0-9]*)\\b","g");
-	html = kwds1.replace(html,"<span class='kwd'>$1</span>");
-	html = vals1.replace(html,"<span class='val'>$1</span>");
+	var functions = new EReg("\\b([a-zA-Z0-9_]+)\\s*\\(","g");
+	html = keywords2EReg.replace(html,"<span class='kwd1'>$1</span>");
+	html = keywords1EReg.replace(html,"<span class='kwd'>$1</span>");
 	html = types.replace(html,"<span class='type'>$1</span>");
+	html = functions.replace(html,"<span class='func'>$1</span>(");
 	html = new EReg("(\"[^\"]*\")","g").replace(html,"<span class='str'>$1</span>");
 	html = new EReg("(//.+?)(\n|$)","g").replace(html,"<span class='cmt'>$1</span>$2");
 	html = new EReg("(/\\*\\*?(.|\n)+?\\*?\\*/)","g").replace(html,"<span class='cmt'>$1</span>");
