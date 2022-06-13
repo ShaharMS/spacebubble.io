b629550905a3e0327f986d846b36e2fe9d64b990
ShaharMS <shaharmsecond@gmail.com>
2022-06-13 11:43:25 +0300

    more search engine freindly

--- haxe/nav.js
+++
@@ -1,7 +0,0 @@
-var navContent = getHTMLFrom("/haxe/navbar.html");
-function getHTMLFrom(path) {
-    var request = new XMLHttpRequest();
-    request.open("GET", path, false);
-    request.send();
-    return request.responseText;
-}
