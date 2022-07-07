069082948af6b55dc1ec7178c166dc6369e5d5c9
ShaharMS <shaharmsecond@gmail.com>
2022-07-07 22:32:45 +0300

    OMG COLLISION WORKS

--- sources/shapeBackground.js
+++ sources/shapeBackground.js
@@ -1,89 +1,154 @@
-let numBalls = 13;
-let spring = 0.05;
-let gravity = 0.00;
-let friction = -0.9;
-let balls = [];
+var canvas;
+var ctx;
 
-function setup() {
-    createCanvas(window.innerWidth, window.innerHeight);
-    for (let i = 0; i < numBalls; i++) {
-        balls[i] = new Ball(
-            random(width),
-            random(height),
-            random(30, 70),
-            i,
-            balls
-        );
+var tempCanvas = document.createElement('canvas');
+var tempCtx = tempCanvas.getContext('2d');
+
+//constants
+let numberOfObjects = 5;
+let baseSize = 75;
+let color = ["#16336c", "#2A63D1"];
+let baseVelocity = 1;
+
+//add a resize event listener to the window
+window.addEventListener('resize', () => {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    tempCanvas.width = window.innerWidth;
+    tempCanvas.height = window.innerHeight;
+}, false);
+
+const getObjectType = () => {
+    let type = Math.floor(Math.random() * 3);
+    switch (type) {
+        case 0:
+            return 'circle';
+        case 1:
+            return 'square';
+        case 2:
+            return 'triangle';
     }
-    noStroke();
-    fill(255, 204);
 }
 
-function draw() {
-    background(0);
-    balls.forEach(ball => {
-        ball.collide();
-        ball.move();
-        ball.display();
+let previousColor = -1;
+
+const getObjectColor = () => {
+    let c = Math.floor(Math.random() * color.length);
+    
+    if (previousColor == c)
+        return getObjectColor();
+    else
+        previousColor = c
+        
+        
+    return color[c]
+
+}
+
+// generate balls
+let objects = Array.from({ length: numberOfObjects }, () => ({
+    x: Math.random() * window.innerWidth,
+    y: Math.random() * window.innerHeight,
+    vx: (Math.random() * baseVelocity * 2) - baseVelocity,
+    vy: (Math.random() * baseVelocity * 2) - baseVelocity,
+    radius: Math.random() * baseSize + baseSize,
+    type: getObjectType(),
+    angularVelocity: ((Math.random() * baseVelocity * 2) - baseVelocity) / 100,
+    color: getObjectColor()
+}));
+
+const getCollisionDirection = (object1, object2) => {
+    let x = object2.x - object1.x;
+    let y = object2.y - object1.y;
+    let radians = Math.atan2(y, x);
+    //return the angle in degrees
+    return radians;
+}
+
+const getVPosFromAngle = (angle) => {
+
+    let vx = Math.cos(angle);
+    let vy = Math.sin(angle);
+
+    return {vx: vx, vy: vy};
+}
+
+const checkCollision = () => {
+    objects.forEach((object1, index1) => {
+        objects.forEach((object2, index2) => {
+            if (index1 != index2) {
+                let distance = Math.sqrt(Math.pow(object1.x - object2.x, 2) + Math.pow(object1.y - object2.y, 2));
+                if (distance < object1.radius + object2.radius) {
+                    let object1AngularVelocity = getCollisionDirection(object1, object2);
+                    let object2AngularVelocity = getCollisionDirection(object2, object1);
+                    //calculate vx and vy based on the angles the objects should be travelling at
+                    object1.vx = getVPosFromAngle(object2AngularVelocity).vx;
+                    object1.vy = getVPosFromAngle(object2AngularVelocity).vy;
+                    object2.vx = getVPosFromAngle(object1AngularVelocity).vx;
+                    object2.vy = getVPosFromAngle(object1AngularVelocity).vy;
+                }
+            }
+        });
     });
 }
 
-class Ball {
-    constructor(xin, yin, din, idin, oin) {
-        this.x = xin;
-        this.y = yin;
-        this.vx = 0;
-        this.vy = 0;
-        this.diameter = din;
-        this.id = idin;
-        this.others = oin;
-    }
+const update = () => {
+    tempCtx.clearRect(0, 0, window.innerWidth, window.innerHeight);
+    objects.forEach((object) => {
+            checkCollision();
+            object.x = object.x + object.vx;
+            object.y = object.y + object.vy;
 
-    collide() {
-        for (let i = this.id + 1; i < numBalls; i++) {
-            // console.log(others[i]);
-            let dx = this.others[i].x - this.x;
-            let dy = this.others[i].y - this.y;
-            let distance = sqrt(dx * dx + dy * dy);
-            let minDist = this.others[i].diameter / 2 + this.diameter / 2;
-            //   console.log(distance);
-            //console.log(minDist);
-            if (distance < minDist) {
-                //console.log("2");
-                let angle = atan2(dy, dx);
-                let targetX = this.x + cos(angle) * minDist;
-                let targetY = this.y + sin(angle) * minDist;
-                let ax = (targetX - this.others[i].x) * spring;
-                let ay = (targetY - this.others[i].y) * spring;
-                this.vx -= ax;
-                this.vy -= ay;
-                this.others[i].vx += ax;
-                this.others[i].vy += ay;
+            if (object.x > window.innerWidth + object.radius) {
+                object.x = 0 - object.radius
+            } else if (object.x < 0 - object.radius) {
+                object.x = window.innerWidth + object.radius
             }
-        }
-    }
 
-    move() {
-        this.vy += gravity;
-        this.x += this.vx;
-        this.y += this.vy;
-        if (this.x + this.diameter / 2 > width) {
-            this.x = width - this.diameter / 2;
-            this.vx *= friction;
-        } else if (this.x - this.diameter / 2 < 0) {
-            this.x = this.diameter / 2;
-            this.vx *= friction;
-        }
-        if (this.y + this.diameter / 2 > height) {
-            this.y = height - this.diameter / 2;
-            this.vy *= friction;
-        } else if (this.y - this.diameter / 2 < 0) {
-            this.y = this.diameter / 2;
-            this.vy *= friction;
-        }
-    }
+            if (object.y > window.innerHeight + object.radius) {
+                object.y = 0 - object.radius
+            } else if (object.y < 0 - object.radius) {
+                object.y = window.innerHeight + object.radius
+            }
+            tempCtx.beginPath();
+            tempCtx.fillStyle = object.color;
+            tempCtx.arc(object.x, object.y, object.radius, 0, 2 * Math.PI);
+            tempCtx.fill();
+        });
 
-    display() {
-        ellipse(this.x, this.y, this.diameter, this.diameter);
-    }
+        let imageData = tempCtx.getImageData(0, 0, window.innerWidth, window.innerHeight);
+
+        ctx.putImageData(imageData, 0, 0);
+
+    requestAnimationFrame(update);
+
+};
+/**
+ * 
+ * @param {Number} objNum The number of balls to be generated
+ * @param {Number} bSize The base size of the balls
+ * @param {string[]} colors  The color of the balls
+ * @param {Number} velocity  The velocity of the balls
+ */
+function startShapeBackground(objNum, bSize, colors, velocity) {
+    numberOfObjects = objNum || numberOfObjects;
+    baseSize = bSize || baseSize;
+    color = colors || color;
+    baseVelocity = velocity;
+
+    canvas = document.getElementById('canvas');
+    ctx = canvas.getContext('2d');
+    ctx.webkitImageSmoothingEnabled = true;
+
+    tempCanvas = document.createElement('canvas');
+    tempCtx = tempCanvas.getContext('2d');
+
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    tempCanvas.width = window.innerWidth;
+    tempCanvas.height = window.innerHeight;
+
+    
+
+    update();
 }
