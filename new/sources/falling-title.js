a464e8204f8677f94c04908b2e73797a67c575ba
ShaharMS <shaharmsecond@gmail.com>
2023-10-31 14:56:24 +0200

    library autocomplete, minor progress

--- new/sources/falling-title.js
+++ new/sources/falling-title.js
@@ -1,10 +1,13 @@
+
 const Engine = Matter.Engine,
-    Render = Matter.Render,
-    Runner = Matter.Runner,
-    Bodies = Matter.Bodies,
-    Composite = Matter.Composite;
+	Render = Matter.Render,
+	Runner = Matter.Runner,
+	Bodies = Matter.Bodies,
+	Composite = Matter.Composite,
+	World = Matter.World;
 
 const engine = Engine.create();
+engine.gravity = 10;
 const world = engine.world;
 
 let section3 = document.getElementById("section-3");
@@ -25,6 +28,33 @@ const section3Elements = document.querySelectorAll("#section-3");
 let bodies = [];
 
 for (const element of section3Elements) {
-	let rigid = Bodies.static(element);
-	bodies.push(rigid);
+
+
+	let rectangles = Array.from(element.getClientRects());
+
+	var vertices = [];
+	for (var i = 0; i < rectangles.length; i++) {
+		var rect = rectangles[i];
+		vertices.push(
+			{ x: rect.left, y: rect.top },
+			{ x: rect.right, y: rect.top },
+			{ x: rect.right, y: rect.bottom },
+			{ x: rect.left, y: rect.bottom }
+		);
+	}
+
+	if (element.id != "section-3-title") {
+		var body = Matter.Bodies.fromVertices(x, y, vertices, {
+			isStatic: true,
+		});
+		Matter.Body.setStatic(body);
+		bodies.push(body); 
+	}
+	else {
+		var body = Matter.Bodies.fromVertices(x, y, vertices, {
+		});
+		bodies.push(body)
+	}
 }
+
+World.add(world, bodies);
