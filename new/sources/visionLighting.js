8c3b07bf0ffba5952309d6acfe18926f681b3624
ShaharMS <shaharmsecond@gmail.com>
2024-06-22 20:34:17 +0300

    spotlights!

--- new/sources/visionLighting.js
+++ new/sources/visionLighting.js
@@ -1,11 +1,28 @@
-
+let lastMouseX = 0; let lastMouseY = 0;
+let section6 = document.getElementById("section-6");
+let searchlight
+/**
+ * Updates the cursor position on the section-6 element.
+ *
+ * @param {Event} e - The event object containing the clientX and clientY coordinates.
+ * @return {void} This function does not return a value.
+ */
 function update(e) {
-    var x = e.clientX || e.touches[0].clientX
-    var y = e.clientY || e.touches[0].clientY
+    var rect = section6.getBoundingClientRect()
+    if (e.type !== "scroll") {
+        lastMouseX = e.clientX
+        lastMouseY = e.clientY
+    }
+    var x = e.clientX || lastMouseX
+    var y = e.clientY || lastMouseY
+    x = x - rect.left
+    y = y - rect.top
+    section6.style.setProperty('--cursorX', x + 'px')
+    section6.style.setProperty('--cursorY', y + 'px')
 
-    document.documentElement.style.setProperty('--cursorX', x + 'px')
-    document.documentElement.style.setProperty('--cursorY', y + 'px')
+    //change angle of searchlights
 }
 
-document.addEventListener('mousemove', update)
-document.addEventListener('touchmove', update)
+section6.addEventListener('mousemove', update)
+section6.addEventListener('touchmove', update)
+window.addEventListener('scroll', update)
